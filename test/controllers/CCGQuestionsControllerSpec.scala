52aecb40fc0c7c8231739bf1f810f893d113b391
brendan.straughan <brendan.straughan@digital.hmrc.gov.uk>
2020-11-11 15:39:39 +0000

    DDCNL-3053 - Added in basic tests for the new CCGQuestionsController

---
+++ test/controllers/CCGQuestionsControllerSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.DataRetrievalAction
+import forms.CCGQuestionsFormProvider
+import models.{CCGQuestions, EOTHOQuestions, FeedbackId, Origin}
+import org.scalatest.concurrent.ScalaFutures
+import views.html.ccgQuestions
+import generators.ModelGenerators
+import navigation.FakeNavigator
+import org.mockito.Matchers.{eq => eqTo, _}
+import org.mockito.Mockito._
+import org.scalacheck.Arbitrary._
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import play.api.data.Form
+import play.api.mvc.Call
+import play.api.test.Helpers._
+import services.AuditService
+
+class CCGQuestionsControllerSpec
+    extends ControllerSpecBase with ScalaCheckPropertyChecks with ModelGenerators with MockitoSugar {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val formProvider = new CCGQuestionsFormProvider()
+  val form = formProvider()
+  lazy val mockAuditService = mock[AuditService]
+
+  def submitCall(origin: Origin) = routes.CCGQuestionsController.onSubmit(origin)
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
+    new CCGQuestionsController(frontendAppConfig, new FakeNavigator(onwardRoute), formProvider, mockAuditService, mcc)
+
+  def viewAsString(form: Form[_] = form, action: Call) =
+    ccgQuestions(frontendAppConfig, form, action)(fakeRequest, messages).toString
+
+  "CCGQuestions Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      forAll(arbitrary[Origin]) { origin =>
+        val result = controller().onPageLoad(origin)(fakeRequest)
+
+        status(result) mustBe OK
+        contentAsString(result) mustBe viewAsString(action = submitCall(origin))
+      }
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+      forAll(arbitrary[Origin]) { origin =>
+        val result = controller().onSubmit(origin)(fakeRequest)
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe Some(onwardRoute.url)
+
+      }
+    }
+  }
+}
