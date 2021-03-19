fd0d40858e5187a209bfe39f4d38701c245dd5c3
Christian-QA <credfernbus@gmail.com>
2021-03-19 16:29:54 +0000

    DDCNL-3643: Fixed incorrect dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,11 +15,10 @@ object AppDependencies {
     "uk.gov.hmrc" %% "play-ui"                       % s"9.0.0-$playVersion",
     "uk.gov.hmrc" %% "http-caching-client"           % s"9.3.0-$playVersion",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % s"1.6.0-$playVersion",
-    "uk.gov.hmrc" %% s"bootstrap-$playVersion"       % "2.3.0",
+    "uk.gov.hmrc" %% s"bootstrap-$playVersion"       % "3.1.0",
     "uk.gov.hmrc" %% "play-language"                 % s"4.11.0-$playVersion")
 
   val test = Seq(
-    guice,
     "uk.gov.hmrc"            %% "hmrctest"           % s"3.9.0-$playVersion",
     "org.scalatest"          %% "scalatest"          % "3.0.4",
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3",
