7d669e12d3f8d9796e26fb847b294e84012da6ea
brendan.straughan <brendan.straughan@digital.hmrc.gov.uk>
2021-06-14 11:54:38 +0100

    DDCNL-4020 - Added in NmwCcgQuestionsFormProviderSpec - Compiling and tests passing

---
+++ test/forms/NmwCcgQuestionsFormProviderSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.{BooleanFieldBehaviours, OptionFieldBehaviours, StringFieldBehaviours}
+import models.NmwCcgQuestions
+import models.ccg.TreatedProfessionallyQuestion
+import play.api.data.FormError
+
+class NmwCcgQuestionsFormProviderSpec
+    extends OptionFieldBehaviours with BooleanFieldBehaviours with StringFieldBehaviours {
+
+  val form = new NmwCcgQuestionsFormProvider()()
+
+  ".treatedProfessionally" must {
+
+    val fieldName = "treatedProfessionally"
+    val invalidError = "error.invalid"
+
+    behave like optionsField[NmwCcgQuestions, TreatedProfessionallyQuestion](
+      form,
+      fieldName,
+      TreatedProfessionallyQuestion.values,
+      FormError(fieldName, invalidError),
+      _.treatedProfessionally
+    )
+  }
+
+}
