df1b127ae00ac38c2feb65f84a64f490b4db0013
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2025-10-17 16:13:17 +0100

    PLATUI-4000: Removed a11y linter tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,20 +2,20 @@ import sbt.*
 
 object AppDependencies {
 
-  val playVersion = "play-30"
-  val frontendVersion = "12.18.0"
+  val playVersion      = "play-30"
+  val frontendVersion  = "12.18.0"
   val bootStrapVersion = "10.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion"                 % frontendVersion,
-    "uk.gov.hmrc" %% s"play-conditional-form-mapping-$playVersion"      % "3.3.0",
-    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion"                 % bootStrapVersion
+    "uk.gov.hmrc" %% s"play-frontend-hmrc-$playVersion"            % frontendVersion,
+    "uk.gov.hmrc" %% s"play-conditional-form-mapping-$playVersion" % "3.3.0",
+    "uk.gov.hmrc" %% s"bootstrap-frontend-$playVersion"            % bootStrapVersion
   )
 
-  val test: Seq[ModuleID]    = Seq(
-    "uk.gov.hmrc"         %% s"bootstrap-test-$playVersion" % bootStrapVersion,
-    "org.jsoup"            % "jsoup"                        % "1.19.1",
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootStrapVersion,
+    "org.jsoup"    % "jsoup"                        % "1.19.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,7 @@
 resolvers += MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefacts.tax.service.gov.uk/maven2")
-resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
+resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(
+  Resolver.ivyStylePatterns
+)
 
 ThisBuild / libraryDependencySchemes += "org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always
 
